a46c06da4891e8dd004ef9843ccf6580a1e9994e
Erika Sequalini <erikasequalini@gmail.com>
2022-10-07 19:34:46 -0300

    Adiciona Templates

--- src/main/java/br/senac/ecommerce/pi/loja/MainController.java
+++ src/main/java/br/senac/ecommerce/pi/loja/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import br.senac.ecommerce.pi.loja.modelo.UsuarioModelo;
 
 @Controller
-//@CrossOrigin(value = "http://localhost:9090")
+// @CrossOrigin(value = "http://localhost:9090")
 public class MainController {
-	
+
 	@GetMapping("/")
 	public String teste() {
 		return "redirect:/home";
@@ -26,25 +26,25 @@ public class MainController {
 
 	@GetMapping("/login")
 	public String verTelaLogin(UsuarioModelo usuario) {
-		if(usuario.getCargos() == null) {
+		if (usuario.getCargos() == null) {
 			return "redirect:/home";
 		}
 		return "login";
 	}
-	
+
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@PostMapping("/error")
-	public Map<String,String> requisao(MethodArgumentNotValidException ex){
-		Map<String,String> problema = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach( (error) -> {
+	public Map<String, String> requisao(MethodArgumentNotValidException ex) {
+		Map<String, String> problema = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
 			String campoNome = ((FieldError) problema).getField();
 			String mensagemErro = error.getDefaultMessage();
-			
+
 			problema.put(campoNome, mensagemErro);
 		});
-		
+
 		return problema;
-		
+
 	}
 }
